075d7bb0c3d29c4a95db5f7b83db211ab9db754f
Porter Smith <flowbish@gmail.com>
2016-06-17 22:44:10 -0700

    build script

--- build.rs
+++ build.rs
@@ -2,8 +2,15 @@
 
 // Bring in a dependency on an externally maintained `gcc` package which manages
 // invoking the C compiler.
+extern crate pkg_config;
 extern crate gcc;
 
 fn main() {
-    gcc::compile_library("libweecord.a", &["src/weecord.c"]);
+    let mut config = gcc::Config::new();
+    let weechat = pkg_config::probe_library("weechat").unwrap();
+    for path in weechat.include_paths {
+        config.include(path);
+    }
+    config.file("src/weecord.c");
+    config.compile("libweecord.a");
 }
