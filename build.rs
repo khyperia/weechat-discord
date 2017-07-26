d3070d5f140c7f9f5dd660ada4be623c4877a795
khyperia <khyperia@live.com>
2017-07-26 16:36:59 -0700

    Improve error message when weechat isn't installed
    Fixes #30

--- build.rs
+++ build.rs
@@ -1,19 +1,22 @@
-// build.rs
-
-// Bring in a dependency on an externally maintained `gcc` package which manages
-// invoking the C compiler.
 extern crate pkg_config;
 extern crate gcc;
 
 fn main() {
+    let weechat = match pkg_config::probe_library("weechat") {
+        Ok(weechat) => weechat,
+        Err(error) => {
+            panic!(format!("Unable to find weechat.pc. Error: [[ {} ]]\n\
+... Ensure that an up-to-date weechat is installed, and if your distro has it, \
+the weechat-dev package as well.",
+                           error))
+        }
+    };
     let mut config = gcc::Config::new();
-    let weechat = pkg_config::probe_library("weechat").unwrap();
     for path in weechat.include_paths {
         config.include(path);
     }
     config.file("src/weecord.c");
     config.flag("-Wall");
     config.flag("-Wextra");
-    config.flag("-Werror");
     config.compile("libweecord.a");
 }
